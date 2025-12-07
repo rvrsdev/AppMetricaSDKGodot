a2d9e2a7cf1e3f11f96eddd79018026b3e1a83a5
rvrsdev <rvrs183@proton.me>
2025-12-07 14:53:34 +0100

    Update AppMetricaGodotPlugin.kt

--- AppMetricaSDK/AppMetricaSDK/src/main/java/com/peerless183/appmetricasdk/AppMetricaGodotPlugin.kt
+++ AppMetricaSDK/AppMetricaSDK/src/main/java/com/peerless183/appmetricasdk/AppMetricaGodotPlugin.kt
@@ -2,30 +2,31 @@ package com.peerless183.appmetricasdk
 
 import io.appmetrica.analytics.AppMetrica
 import android.content.Context
+import android.util.Log
 import io.appmetrica.analytics.AppMetricaConfig
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
 
 class AppMetricaGodotPlugin(godot: Godot?) : GodotPlugin(godot) {
-    lateinit var config: AppMetricaConfig.Builder
+    private var isInitialized = false
 
     override fun getPluginName(): String {
         return "AppMetricaSDK"
     }
 
     @UsedByGodot
-    fun config(apiKey: String) {
-        config = AppMetricaConfig.newConfigBuilder(apiKey)
-    }
-
-
-    @UsedByGodot
-    fun init() {
+    fun init(apiKey: String) {
         if (godot == null) return
 
-        AppMetrica.activate(activity as Context, config.build())
-        android.util.Log.d("AppMetrica", "AppMetrica initialized")
+        try {
+            val config = AppMetricaConfig.newConfigBuilder(apiKey).build()
+            AppMetrica.activate(activity as Context, config)
+            isInitialized = true
+            Log.d("AppMetrica", "AppMetrica initialized successfully")
+        } catch (e: Exception) {
+            Log.e("AppMetrica", "Failed to initialize: ${e.message}")
+        }
     }
 
 
@@ -39,6 +40,19 @@ class AppMetricaGodotPlugin(godot: Godot?) : GodotPlugin(godot) {
         activity?.let {
             AppMetrica.reportAppOpen(it)
         }
+    }
+
+    @UsedByGodot
+    fun reportEventWithParams(name: String, par1: Any, par2: Any, par3: Any){
+        if(!isInitialized) return
+
+        val params = mapOf<String, Any>(
+            "par1" to par1,
+            "par2" to par2,
+            "par3" to par3
+        )
+        AppMetrica.reportEvent(name, params)
+//      AppMetrica.sendEventsBuffer() - Опционально
     }
 
 }
